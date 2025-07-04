ab62c4dd3e21b036e5f00bcc0d6a4b74f32a4462
Jakub Sowa <sowa.jakub@gmail.com>
2025-07-04 12:16:01 +0200

    project init

---
+++ FediApp/FediAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FediAppApp.swift
+//  FediApp
+//
+//  Created by Jakub Sowa on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FediAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
